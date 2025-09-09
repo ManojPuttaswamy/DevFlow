3111275b82ebce206740bfc245c4eb193e2342ee
Manoj Puttaswamy <manoj.puttaswamy.c@gmail.com>
2025-09-09 14:48:47 -0500

    Dockerfile frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      // Disable ESLint during builds (not recommended for production)
-      ignoreDuringBuilds: true,
-    },
-    typescript: {
-      // Keep TypeScript checking
-      ignoreBuildErrors: false,
-    }
-  };
-  
-  module.exports = nextConfig;
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: undefined,
+  },
+  eslint: {
+    // Disable ESLint during builds (not recommended for production)
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Keep TypeScript checking
+    ignoreBuildErrors: false,
+  }
+};
+
+module.exports = nextConfig;
